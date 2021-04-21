cd0962db09743128ff0489a0a7297b84ba8170ed
Joël Séguillon <j.seguillon@ateme.com>
2021-04-21 22:32:24 +0200

    rework => using now dedicated CstUtils class

--- src/components/ParseError.js
+++ src/components/ParseError.js
@@ -1,4 +1,5 @@
 const monaco = require('monaco-editor')
+import CstUtils from '../components/CstUtils.js'
 
 export default class ParseError {
   message = null
@@ -43,7 +44,7 @@ export default class ParseError {
         }
         else if (currErr.type === "remove" ) {
           //TODO : set a hover for base64 decode proposal for data secrets
-          //TODO : should not ignore but test resources named cpu and memory
+          //FIXME : should not ignore all but test resources named cpu and memory
           var matches = [...currErr.message.dataPath.matchAll(/\/data\/.*|\/spec\/selector.*|.*\/spec\/clusterIPs.*|.*\/metadata\/labels.*|.*\/metadata\/annotations.*|.*\/spec\/nodeSelector.*|.*\/resources\/limits.*|.*\/resources\/requests.*/g)]
           if (matches.length == 0){
             filteredErrors.push(currErr)
@@ -122,95 +123,12 @@ export default class ParseError {
   getPosition(cstDoc, fullStringDoc) {
     if (this.type !== "yaml" ) {
       try {
-        return this.getCharPosition(this.findNode(this.convertJsonPath(this.message.dataPath),cstDoc).range, cstDoc, fullStringDoc)
-
+        console.log("<hat",CstUtils.findNode(CstUtils.convertJsonPath(this.message.dataPath),cstDoc)[0].range, cstDoc, fullStringDoc)
+        return CstUtils.getCharPosition(CstUtils.findNode(CstUtils.convertJsonPath(this.message.dataPath),cstDoc)[0].range, cstDoc, fullStringDoc)
       } catch(e) {console.log("could not find path for: ", e)}
     }
     else {
-      return this.getCharPosition(this.message.source.range, cstDoc, fullStringDoc)
-    }
-  }
-
-  getCharPosition(position, doc, fullStringDoc){
-    //seach first \n after position.start
-    var endOfLine = fullStringDoc.toString().indexOf("\n", position.start)
-    //stop the doc at line of error
-    var slicedDoc=fullStringDoc.toString().slice(0,endOfLine).split(/\n/)
-
-    //Search for non whitespace chars
-    var lastLine=slicedDoc[slicedDoc.length-1]
-    var matches = [...lastLine.matchAll(/\S|$/g)];
-
-    //compute
-    var startColumn
-    var endColumn
-    if (matches.length > 0){
-      startColumn = matches[0].index
-      endColumn = matches[matches.length-1].index
-    } else {
-      startColumn = 0
-      endColumn = lastLine.length
-    }
-
-    return { "relativeLine": doc.toString().slice(0,position.start-doc.valueRange.start+1).split(/\n/).length,
-             "absoluteLine": slicedDoc.length,
-             "startLine": slicedDoc.length,
-             "startColumn": startColumn+1,
-             "endColumn": endColumn+1
-            }
-  }
-  // if (errors.length > 0) {
-  //   errors=this.setLineForErrors(errors, parsed[i], newVal)
-  //   allErrors=allErrors.concat(errors)
-  // }
-  findNode(key, elements, parent) {
-    if (elements === undefined || elements === null) {
-        return parent
-    }
-
-    // Very first level : can only be an array
-    if (elements.type === "DOCUMENT") {
-      //only deal with Collections
-      for (var i=0; i < elements.contents.length; i++){
-        if (elements.contents[i].type === "MAP" ){
-          return this.findNode(key, elements.contents[i], elements)
-        }
-      }
-    }
-
-    let parsedKeys = key.split("/")
-
-    // went at end of tree => return
-    if (elements.type === "PLAIN") {
-        return elements
-    }
-    //Iterate the MAP ensuring staying on the path we search
-    if (elements.type === "MAP") {
-        let items = elements.items
-        for (let i=0; i < items.length; i++) {
-            if (items[i].strValue === parsedKeys[0]) {
-                // end on the jsonpath
-                if (parsedKeys.length == 1) {
-                  return items[i]
-                }
-                // still some part of path to solve
-                if (items[i+1].type === "MAP_VALUE" ) { // && items[i+1].node !== undefined
-                    return this.findNode(parsedKeys.slice(1).join("/"), items[i+1].node, elements)
-                 }
-                return
-            }
-        }
-        return
-    }
-    if (elements.type === "SEQ") {
-        const index = parseInt(parsedKeys[0], 10)
-        let items = elements.items
-
-        if (items[index] !== undefined){
-          return this.findNode(parsedKeys.slice(1).join("/"), items[index].node, elements)
-        } else {
-          return parent
-        }
+      return  CstUtils.getCharPosition(this.message.source.range, cstDoc, fullStringDoc)
     }
   }
 }
