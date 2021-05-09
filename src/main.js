f143409d8ef3586542cba6b37d0cfe460f00ad8d
Joël Séguillon <j.seguillon@ateme.com>
2021-05-09 22:49:13 +0200

    fix missing tweet feed package and main import

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import VueClipboard from 'vue-clipboard2'
 import VueSocialSharing from 'vue-social-sharing'
-
 Vue.use(VueSocialSharing);
+import TwitterFeed from "vuejs-twitter-feed";
+Vue.use(TwitterFeed);
 
 Vue.config.productionTip = false;
 
